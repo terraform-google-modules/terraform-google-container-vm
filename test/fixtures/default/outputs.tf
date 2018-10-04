aadefa05fe190b2bf809c2f243dcb783d81baeaf
Adrien Thebo <adrien@lagrange-automation.io>
2018-10-04 14:23:19 -0700

    Plump test fixture variables to outputs
    The `terraform` verifier will transfer Terraform outputs into Inspec as
    attributes. However, we were depending on the value of those variables
    to run tests against the Terraform instantiated resources, and didn't
    have access to that data.
    
    This commit solves the issue by passing all Terraform variables back out
    as outputs, so that the InSpec tests can easily reference that data.

---
+++ test/fixtures/default/outputs.tf
@@ -0,0 +1,36 @@
+output "project_id" {
+  value = "${var.project_id}"
+}
+
+output "subnetwork_project" {
+  value = "${var.subnetwork_project}"
+}
+
+output "subnetwork" {
+  value = "${var.subnetwork}"
+}
+
+output "credentials_path" {
+  value = "${var.credentials_path}"
+}
+
+output "instance_name" {
+  value = "${var.instance_name}"
+}
+
+output "image" {
+  value = "${var.image}"
+}
+
+output "machine_type" {
+  value = "${var.machine_type}"
+}
+
+output "region" {
+  value = "${var.region}"
+}
+
+output "zone" {
+  value = "${var.zone}"
+}
+
