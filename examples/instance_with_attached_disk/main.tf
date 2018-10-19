0a8928a7807ffcfa8217c4bd5ed21fa71b32fa01
Jason Berlinsky <jason@barefootcoders.com>
2018-10-19 16:58:00 -0400

    Remove extraneous service account scopes from examples

--- examples/instance_with_attached_disk/main.tf
+++ examples/instance_with_attached_disk/main.tf
@@ -115,12 +115,6 @@ resource "google_compute_instance" "vm" {
 
   service_account {
     scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring.write",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/trace.append",
       "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
--- examples/managed_instance_group/main.tf
+++ examples/managed_instance_group/main.tf
@@ -65,12 +65,6 @@ module "mig" {
   }
 
   service_account_scopes = [
-    "https://www.googleapis.com/auth/devstorage.read_only",
-    "https://www.googleapis.com/auth/logging.write",
-    "https://www.googleapis.com/auth/monitoring.write",
-    "https://www.googleapis.com/auth/servicecontrol",
-    "https://www.googleapis.com/auth/service.management.readonly",
-    "https://www.googleapis.com/auth/trace.append",
     "https://www.googleapis.com/auth/cloud-platform",
   ]
 
